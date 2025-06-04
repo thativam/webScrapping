797f97ceb367b6587da0b22312ac820e6ded8b32
thativam <thativancapeli@outlook.com>
2025-06-04 19:24:08 -0300

    feat: initial commit

---
+++ src/test/java/com/personal/webscrapping/WebScrappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.webscrapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
